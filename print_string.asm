549c070eaf62502a4737097967588c6169f4aacd
Fernando Barboza <falandocomofernando@gmail.com>
2023-11-15 22:14:43 -0300

    improve print string func

--- print_string.asm
+++ print_string.asm
@@ -1,8 +1,14 @@
 ; RSI -> should contain the string.
 ; RDX -> string length
 print_string:
+  push rax
+  push rdi
+
   mov rax, 1          ; System call for write
   mov rdi, 1          ; File descriptor 1 (stdout)
   syscall
+
+  pop rax
+  pop rdi
   ret
 
